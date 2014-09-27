122c32d0f25047afd39c91eeba9d68f42566ad6e
Luke Benstead <kazade@gmail.com>
2014-09-27 09:06:01 +0100

    Make everything compile as a shared library

--- Android.mk
+++ Android.mk
@@ -2,44 +2,42 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-OPENAL_DIR := OpenAL
+LOCAL_MODULE    := openal
 
 AL_SOURCES := \
-  $(OPENAL_DIR)/Alc/android.c              \
-  $(OPENAL_DIR)/OpenAL32/alAuxEffectSlot.c \
-  $(OPENAL_DIR)/OpenAL32/alBuffer.c        \
-  $(OPENAL_DIR)/OpenAL32/alDatabuffer.c    \
-  $(OPENAL_DIR)/OpenAL32/alEffect.c        \
-  $(OPENAL_DIR)/OpenAL32/alError.c         \
-  $(OPENAL_DIR)/OpenAL32/alExtension.c     \
-  $(OPENAL_DIR)/OpenAL32/alFilter.c        \
-  $(OPENAL_DIR)/OpenAL32/alListener.c      \
-  $(OPENAL_DIR)/OpenAL32/alSource.c        \
-  $(OPENAL_DIR)/OpenAL32/alState.c         \
-  $(OPENAL_DIR)/OpenAL32/alThunk.c         \
-  $(OPENAL_DIR)/Alc/ALc.c                  \
-  $(OPENAL_DIR)/Alc/alcConfig.c            \
-  $(OPENAL_DIR)/Alc/alcEcho.c              \
-  $(OPENAL_DIR)/Alc/alcModulator.c         \
-  $(OPENAL_DIR)/Alc/alcReverb.c            \
-  $(OPENAL_DIR)/Alc/alcRing.c              \
-  $(OPENAL_DIR)/Alc/alcThread.c            \
-  $(OPENAL_DIR)/Alc/ALu.c                  \
-  $(OPENAL_DIR)/Alc/bs2b.c                 \
-  $(OPENAL_DIR)/Alc/null.c                 \
-  $(OPENAL_DIR)/Alc/panning.c              \
-  $(OPENAL_DIR)/Alc/mixer.c                \
-  $(OPENAL_DIR)/Alc/audiotrack.c           \
-  $(OPENAL_DIR)/Alc/opensles.c
+  $(LOCAL_PATH)/OpenAL/Alc/android.c              \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alAuxEffectSlot.c \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alBuffer.c        \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alDatabuffer.c    \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alEffect.c        \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alError.c         \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alExtension.c     \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alFilter.c        \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alListener.c      \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alSource.c        \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alState.c         \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/alThunk.c         \
+  $(LOCAL_PATH)/OpenAL/Alc/ALc.c                  \
+  $(LOCAL_PATH)/OpenAL/Alc/alcConfig.c            \
+  $(LOCAL_PATH)/OpenAL/Alc/alcEcho.c              \
+  $(LOCAL_PATH)/OpenAL/Alc/alcModulator.c         \
+  $(LOCAL_PATH)/OpenAL/Alc/alcReverb.c            \
+  $(LOCAL_PATH)/OpenAL/Alc/alcRing.c              \
+  $(LOCAL_PATH)/OpenAL/Alc/alcThread.c            \
+  $(LOCAL_PATH)/OpenAL/Alc/ALu.c                  \
+  $(LOCAL_PATH)/OpenAL/Alc/bs2b.c                 \
+  $(LOCAL_PATH)/OpenAL/Alc/null.c                 \
+  $(LOCAL_PATH)/OpenAL/Alc/panning.c              \
+  $(LOCAL_PATH)/OpenAL/Alc/mixer.c                \
+  $(LOCAL_PATH)/OpenAL/Alc/audiotrack.c           \
+  $(LOCAL_PATH)/OpenAL/Alc/opensles.c
 
-
-LOCAL_MODULE    := openal
-LOCAL_SRC_FILES := $(AL_SOURCES)
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := \
-  $(HOME)/OpenAL \
-  $(HOME)/OpenAL/include \
-  $(HOME)/OpenAL/OpenAL32/Include \
+  $(LOCAL_PATH)/OpenAL \
+  $(LOCAL_PATH)/OpenAL/include \
+  $(LOCAL_PATH)/OpenAL/OpenAL32/Include \
 
 
 LOCAL_CFLAGS += \
@@ -52,4 +50,4 @@ MAX_SOURCES_HIGH ?= 64
 LOCAL_CFLAGS += -DMAX_SOURCES_LOW=$(MAX_SOURCES_LOW) -DMAX_SOURCES_START=$(MAX_SOURCES_START) -DMAX_SOURCES_HIGH=$(MAX_SOURCES_HIGH)
 LOCAL_CFLAGS += -DPOST_FROYO
 
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
